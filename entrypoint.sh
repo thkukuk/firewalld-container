1a56516ca0f37d5797286056f07d76afc57045a7
Thorsten Kukuk <kukuk@suse.com>
2022-03-08 19:33:36 +0100

    Try running entrypoint script from catatonit

--- entrypoint.sh
+++ entrypoint.sh
@@ -36,7 +36,7 @@ stop_daemons() {
 start_daemons() {
     mkdir -p /run/dbus
     /usr/bin/dbus-daemon --system --fork
-    exec catatonit -- /usr/sbin/firewalld --nofork $FIREWALLD_ARGS
+    /usr/sbin/firewalld --nofork $FIREWALLD_ARGS
 }
 
 #
