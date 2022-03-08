ee69ec87a50afd3a73f3ba4cacf95c9b327e85ee
Thorsten Kukuk <kukuk@suse.com>
2022-03-08 09:58:32 +0100

    New entrypoint file to start/stop additional dbus-broker

---
+++ entrypoint.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+
+DEBUG=${DEBUG:-"0"}
+
+[ "${DEBUG}" = "1" ] && set -x
+
+export PATH=/usr/sbin:/sbin:${PATH}
+
+terminate() {
+    base=$(basename "$1")
+    pid=$(/bin/pidof "$base")
+
+    if [ -n "$pid" ]; then
+	echo "Terminating $base..."
+	if kill "$pid" ; then
+	    echo "Terminating $base failed!"
+	fi
+    else
+	echo "Failure determining PID of $base"
+    fi
+}
+
+init_trap() {
+    trap stop_daemons TERM INT
+}
+
+stop_daemons() {
+    terminate /usr/bin/dbus-broker
+    terminate /usr/sbin/firewalld
+}
+
+start_daemons() {
+    mkdir /run/dbus
+    dbus-broker --system --fork
+    "$@"
+}
+
+#
+# Main
+#
+
+init_trap
+
+if [ "$1" = 'firewalld' ]; then
+    start_daemons "$@"
+    echo "firewalld running and ready"
+    /usr/bin/pause
+else
+    exec "$@"
+fi
